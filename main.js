0c7e6381b189373be9769c3ed14904ab963a9c4f
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-12 11:14:23 -0800

    bounce off the walls

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("myCanvas"); // storing a reference to the <canvas> element to the canvas variable
 const ctx = canvas.getContext("2d"); // ctx variable to store the 2D rendering context
+const ballRadius = 10;
 
 let x = canvas.width / 2;
 let y = canvas.height - 30;
@@ -9,7 +10,7 @@ let dy = -2;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -20,6 +21,13 @@ function drawBall() {
     drawBall();
     x += dx;
     y += dy;
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+      if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
 }
 
 setInterval(draw, 10);
