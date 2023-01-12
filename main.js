c09926f3591e14ee84805364b9f08ab18cc85a74
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-12 13:12:51 -0800

    mouse controls

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ for (let c = 0; c < brickColumnCount; c++) {
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 function keyDownHandler(e) {
     if (e.key === "Right" || e.key === "ArrowRight") {
@@ -45,6 +46,14 @@ function keyUpHandler(e) {
       rightPressed = false;
     } else if (e.key === "Left" || e.key === "ArrowLeft") {
       leftPressed = false;
+    }
+}
+
+// handler function that updates the paddle position based on the pointer coordinates
+function mouseMoveHandler(e) {
+    const relativeX = e.clientX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+      paddleX = relativeX - paddleWidth / 2;
     }
 }
 
