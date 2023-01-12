daf05edffc3b184a9a10b10edc7a58ff57fddcec
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-12 11:26:00 -0800

    paddle & keyboard controls

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const canvas = document.getElementById("myCanvas"); // storing a reference to the <canvas> element to the canvas variable
 const ctx = canvas.getContext("2d"); // ctx variable to store the 2D rendering context
-const ballRadius = 10;
+const ballRadius = 10; // hold the radius of the drawn circle
+const paddleHeight = 10;
+const paddleWidth = 75;
+
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
 
 let x = canvas.width / 2;
 let y = canvas.height - 30;
@@ -15,18 +21,52 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
 }
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
   
-  function draw() {
+function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
     x += dx;
     y += dy;
 
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-      if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
         dy = -dy;
+    }
+
+    if (rightPressed) {
+        paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+    } else if (leftPressed) {
+        paddleX = Math.max(paddleX - 7, 0);
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = true;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = true;
+    }
+}
+  
+function keyUpHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = false;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = false;
     }
 }
 
