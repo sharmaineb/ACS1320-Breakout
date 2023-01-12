c7946fdb627b418c858b7c1c922aabc92163651d
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-12 11:04:57 -0800

    move the ball

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 const canvas = document.getElementById("myCanvas"); // storing a reference to the <canvas> element to the canvas variable
 const ctx = canvas.getContext("2d"); // ctx variable to store the 2D rendering context
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50); // defining a rectangle
-ctx.fillStyle = "#a8e5d2"; // stores a color
-ctx.fill(); // paints the square
-ctx.closePath();
+let x = canvas.width / 2;
+let y = canvas.height - 30;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+let dx = 2;
+let dy = -2;
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+  
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
