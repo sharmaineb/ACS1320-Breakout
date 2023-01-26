954a80acbdc57de07a36c463b7490d411d72d23e
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-26 00:16:46 -0800

    [update] cont. working

---
+++ Ball.js
@@ -0,0 +1,23 @@
+class Ball {
+  constructor(x = 0, y = 0, dx = 2, dy = -1, radius = 10, color = '#ff6f69') {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  move() {
+    this.x += this.dx;
+    this.y += this.dy;
+  }
+
+  render(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, PI2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+}
