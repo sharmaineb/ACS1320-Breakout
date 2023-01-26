677211082e6120c3b69b8ed6c34dc440b0037900
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-01-26 12:40:33 -0800

    [update] bundle

---
+++ src/Ball.js
@@ -0,0 +1,25 @@
+import Sprite from './Sprite';
+
+class Ball extends Sprite {
+  constructor(x = 0, y = 0, dx = 2, dy = -1, radius = 10, color = '#ff6f69') {
+    super(x, y, radius * 2, radius * 2, color);
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.PI2 = Math.PI * 2;
+  }
+
+  move() {
+    this.moveBy(this.dx, this.dy);
+  }
+
+  render(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, this.PI2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+export default Ball;
---
+++ src/Brick.js
@@ -0,0 +1,11 @@
+import Sprite from './Sprite';
+
+class Brick extends Sprite {
+  constructor(x, y, width, height, color) {
+    super(x, y, width, height, color);
+
+    this.status = 1;
+  }
+}
+
+export default Brick;
